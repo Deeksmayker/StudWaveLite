18251438fbdfca06d32e8a152d053d5f066a64d4
Dmitry Tiulenev <lokkins334@gmail.com>
2021-04-10 23:59:18 +0500

    Добавил класс даты и времени

---
+++ StudWaveLite/Model/GameDateTime.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudWaveLite.Model
+{
+    public static class GameDateTime
+    {
+        private static int month = 9;
+
+        public static int Month
+        {
+            get => month;
+            set
+            {
+                if (value > 12)
+                {
+                    value -= 12;
+                    year++;
+                }
+
+                month = value;
+            }
+        }
+
+        private static int year = 2021;
+
+        public static int Year
+        {
+            get => year;
+            set => throw new Exception("Зачем меняешь год");
+        }
+
+        private static int hour;
+
+        public static int Hour
+        {
+            get => hour;
+            set
+            {
+                if (value >= 24)
+                    value -= 24;
+                hour = value;
+            }
+        }
+
+        private static int minute;
+
+        public static int Minute
+        {
+            get => minute;
+            set
+            {
+                if (value >= 60)
+                {
+                    value -= 60;
+                    hour++;
+                }
+
+                minute = value;
+            }
+        }
+    }
+}
---
+++ StudWaveLite/Model/World.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudWaveLite.Model
+{
+    class World
+    {
+        private DateTime date = new DateTime(2021, 9, 1, 0, 0, 0);
+    }
+}
