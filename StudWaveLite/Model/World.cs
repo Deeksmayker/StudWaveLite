6be4a7f57bfb99802a77cb3e3c04aed9caa0cda8
Dmitry Tiulenev <lokkins334@gmail.com>
2021-04-11 15:14:29 +0500

    Немного прописал выборы в ворлде

--- StudWaveLite/Model/World.cs
+++ StudWaveLite/Model/World.cs
@@ -6,8 +6,21 @@ using System.Threading.Tasks;
 
 namespace StudWaveLite.Model
 {
-    class World
+    public class World
     {
-        
+        private int sportStat;
+
+        public int SportStat
+        {
+            get => sportStat;
+            set
+            {
+                if (value - sportStat > 1)
+                    throw new Exception("Выбор за спорт насчитался больше одного");
+                if (value < 0)
+                    value = 0;
+                sportStat = value;
+            }
+        }
     }
 }
